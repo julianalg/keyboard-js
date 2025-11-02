26b95ef0632d48af3ae63b3dc26a5dbfecd524e8
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2025-11-02 09:56:24 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Step 0: Look at the HTML and CSS files to understand the structure and styling of the piano keys. Is there anything that confuses you there?
+
+// Step 1: Using the DOM, select all of the piano key elements
+
+
+
+
+
+
+// Step 2: Using event listeners, on click of each key, play the corresponding sound
